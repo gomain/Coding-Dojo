f9f821e4b25f0ef73cae0d43830033e03981abe3
Joe Gomain Hoyes <gomain@gmail.com>
2020-03-26 12:26:03 +0700

    grade: abstract Rules

--- src/Grade.purs
+++ src/Grade.purs
@@ -4,8 +4,9 @@ import Prelude
 
 import Data.Foldable (find)
 import Data.Maybe (maybe)
+import Data.Newtype (class Newtype, un)
 import Data.Tuple (fst, snd)
-import Data.Tuple.Nested ((/\))
+import Data.Tuple.Nested (type (/\), (/\))
 
 newtype Grade = Grade String
 
@@ -14,11 +15,28 @@ instance showGrade :: Show Grade where
 
 type Mark = Int
 
+newtype Rules = Rules (Array (Grade /\ Mark))
+
+derive instance newtypeRules :: Newtype Rules _
+
 grade :: Mark -> Grade
-grade mark = maybe (Grade "F") fst <<< find ((_ <= mark) <<< snd) $ rules where
-  rules =
-    [ (Grade "A" /\ 91)
-    , (Grade "B" /\ 81)
-    , (Grade "C" /\ 71)
-    , (Grade "D" /\ 61)
-    ]
+grade = grade' rules
+
+rules :: Rules
+rules = Rules
+  [ (Grade "A" /\ 91)
+  , (Grade "B" /\ 81)
+  , (Grade "C" /\ 71)
+  , (Grade "D" /\ 61)
+  ]                  
+
+grade' :: Rules -> Mark -> Grade
+grade' rules' mark = maybe (Grade "F") fst <<< find ((_ <= mark) <<< snd) <<< un Rules $ rules'
+  
+  
+  
+  
+  
+  
+
+
